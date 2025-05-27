0a2f299b2665b11bb643c1ae99a85423b00962db
hyeonpyo <gusvy0220@gmail.com>
2025-05-28 01:52:03 +0900

    feat: 프로젝트 세팅

---
+++ wallpadcontroller/src/main/java/com/hyeonpyo/wallpadcontroller/WallpadcontrollerApplication.java
@@ -0,0 +1,13 @@
+package com.hyeonpyo.wallpadcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallpadcontrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WallpadcontrollerApplication.class, args);
+	}
+
+}
