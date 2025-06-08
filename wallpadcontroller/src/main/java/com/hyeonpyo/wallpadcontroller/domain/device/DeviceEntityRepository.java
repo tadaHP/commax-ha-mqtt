55f0fff3c3994b5e6ce4fa773d38e2aedb30cf42
홍현표 <gusvy0220@gmail.com>
2025-06-08 12:46:16 +0900

    Revert "feat: 기능을 기존에 commax.yaml 구조에서 가져오던 내용을 DB에서 가져오도록 수정 (#1)" (#2)
    This reverts commit 14d035808364768780b8d4c675f1ff15df3959b4.

--- wallpadcontroller/src/main/java/com/hyeonpyo/wallpadcontroller/domain/device/DeviceEntityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hyeonpyo.wallpadcontroller.domain.device;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeviceEntityRepository extends JpaRepository<DeviceEntity, String> {
-    boolean existsByUniqueId(String uniqueId);
-}
